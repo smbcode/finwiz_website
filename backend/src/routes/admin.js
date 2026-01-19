f8ba2971c33e2276405f3662e6d8c22cdb6db630
Your Name <shauank.bangale14@gmail.com>
2026-01-20 01:39:02 +0530

    fixing admin

--- backend/src/routes/admin.js
+++ backend/src/routes/admin.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Event = require("../models/Event");
+const Event = require("../../models/Event");
 const adminMiddleware = require("../middleware/adminMiddleware");
 
 // All routes require admin authentication
--- backend/src/routes/events.js
+++ backend/src/routes/events.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Event = require("../models/Event");
+const Event = require("../../models/Event");
 
 // GET all events (public route)
 router.get("/", async (req, res) => {
